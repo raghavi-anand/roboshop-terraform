9e3451be8e8df38c154948aa167054a79c7045b4
raghavi-anand <raghavianandkumar@gmail.com>
2023-08-19 02:31:05 +0000

    v

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
-terraform {
-  backend "s3" {}
-}
- 
-variable "test"{}
-
-output "test"{
-    value = var.test
+module "components" {
+  source   = "git::https://github.com/raghavi-anand/test-module-basic.git"
+  for_each = var.components
+  zone_id             = var.zone_id
+  security_groups     = var.security_groups
+  name                = each.value["name"]
+  instance_type       = each.value["instance_type"]
 }
