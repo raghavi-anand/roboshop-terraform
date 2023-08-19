570b2ba5213126270ae042d04caa4a41af67f8f0
raghavi-anand <raghavianandkumar@gmail.com>
2023-08-19 03:32:21 +0000

    z

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 module "components" {
-  source              = "git::https://github.com/raghavi-anand/test-module-basic.git"
+  source              = "git::https://github.com/raghavi-anand/tf-module-basic.git"
+  
   for_each            = var.components
+  
   zone_id             = var.zone_id
   security_groups     = var.security_groups
   name                = each.value["name"]
